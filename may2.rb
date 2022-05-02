8bef09b6aaf601fa69ae5bfa0df5257f95ad13ac
Matthew Benitez <matthew.benitez@appfolio.com>
2022-05-02 09:16:52 -0700

    Add Problem for May 2

---
+++ may2.rb
@@ -0,0 +1,53 @@
+# May 2, 2022 [Easy]
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+# Bonus: Can you do this in one pass?
+
+# time complexity is find * n
+def list_sum_k(arr, k)  
+  arr.each do |num|
+    if arr.find { |num2| num + num2 == k } 
+      return true
+    end
+  end
+
+  false
+end
+
+# time complexity is that of the sort
+def list_sum_k_in_place_compare(arr, k)
+  sorted = arr.sort
+
+  lower_index = 0;
+  upper_index = sorted.length - 1
+
+  while lower_index < upper_index
+    sum = sorted[lower_index] + sorted[upper_index]
+
+    if sum < k
+      lower_index += 1
+    elsif sum > k 
+      upper_index -= 1
+    else
+      return true
+    end
+  end
+
+  false
+end
+
+test_cases = [
+  {arr: [1, 6, 2, 8, 11, 9], target: 14, expected: true},
+  {arr: [0, 7, 3, 14, 6, 11], target: 6, expected: true},
+  {arr: [-30, -20, -10, 0, 10, 60, 45, 30], target: 50, expected: true},
+  {arr: [7, 20, -30, 5, 16, -20], target: -10, expected: true},
+  {arr: [1, 2, 3, 4], target: 15, expected: false},
+  {arr: [-30, -10, -20, -15], target: 5, expected: false},
+  {arr: [-5, -4, -2, 2, 4, 5], target: 5, expected: false},
+  {arr: [-5, -4, -2, 2, 4, 5], target: 5, expected: false},
+].freeze
+
+test_cases.each do |test| 
+  puts "Error" if list_sum_k(test[:arr], test[:target]) != test[:expected]
+  puts "Error" if list_sum_k_in_place_compare(test[:arr], test[:target]) != test[:expected]
+end
---
+++ may4.rb
@@ -0,0 +1,35 @@
+# Given the root to a binary tree, implement serialize(root), which serializes
+# the tree into a string, and deserialize(s), which deserializes the string 
+# back into the tree.
+#
+# For example, given the following Node class
+# class Node:
+#     def __init__(self, val, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#
+# The following test should pass:
+# node = Node('root', Node('left', Node('left.left')), Node('right'))
+# assert deserialize(serialize(node)).left.left.val == 'left.left'
+#
+class Node
+  attr_accessor :val, :left, :right
+  def initialize(val, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
+
+def deserialize(node)
+  node
+end
+
+def serialize(node)
+  node
+end
+
+# node = Node.new('root')
+node = Node.new('root', Node.new('left', Node.new('left.left')), Node.new('right'))
+puts deserialize(serialize(node)).left.left.val == 'left.left'
