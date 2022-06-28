c0b171d42011dcefcddac908cce44d73e07ee809
Matthew Benitez <matthew.benitez@appfolio.com>
2022-06-28 11:55:19 -0700

    Add June 28

---
+++ jun28.py
@@ -0,0 +1,27 @@
+# Given an array of integers, return a new array such that each element at
+# index i of the new array is the product of all the numbers in the original
+# array except the one at i.
+
+# For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+# [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would
+# be [2, 3, 6].
+
+# Follow-up: what if you can’t use division?
+
+from functools import reduce
+
+def generate_products(nums):
+  products = [0] * len(nums)
+  total = reduce((lambda x, y: x * y), nums)
+  for idx, x in enumerate(nums):
+    products[idx] = round(total / x)
+
+  return products
+
+def main():
+  arr = [1, 2, 3, 4, 5]
+  products = generate_products(arr)
+  print(products)
+
+if __name__ == "__main__":
+  main()
